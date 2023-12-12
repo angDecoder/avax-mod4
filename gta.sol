8289ab9753d4b308f7bb79bb222169f956b882ee
Angshu Das <angshudas012@gmail.com>
2023-12-12 12:41:10 +0530

    Update gta.sol

--- gta.sol
+++ gta.sol
@@ -6,19 +6,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GrandTheftAuto is ERC20, Ownable(msg.sender) {
 
-    uint public constant LootAmount = 999;
+    uint public constant Gift = 10;
+    mapping(address => bool) private GiftRedeemed;
+
 
     constructor() ERC20("GrandTheftAuto", "GTA") {
-        _mint(msg.sender, LootAmount);
+        _mint(msg.sender, Gift);
     }
 
     function mintLoot(address account, uint amount) public onlyOwner {
         _mint(account, amount);
     }
 
-    function burnLoot(uint amount) public {
-        require(balanceOf(msg.sender) >= amount, "Balance is insufficient");
+    function redeemItem() public {
 
+        require(balanceOf(msg.sender) >= Gift, "Balance is insufficient");
+
+        require(!GiftRedeemed[msg.sender], "Gift redeemed already");
+
+        GiftRedeemed[msg.sender] = true;
+        _burn(msg.sender, Gift);
+    }
+
+   function burnLoot(uint amount) public {
         _burn(msg.sender, amount);
     }
 
